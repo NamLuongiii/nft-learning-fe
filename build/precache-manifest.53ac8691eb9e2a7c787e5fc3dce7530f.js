a690dc6ad59ae938479c6575286e89fb6bb0085c
Lương Khắc Nam <namlk@hamsa.co>
2022-03-11 10:29:49 +0700

    upload ./build to github

---
+++ build/precache-manifest.53ac8691eb9e2a7c787e5fc3dce7530f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848847c749918b403eb0e1fecfd59159",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92a9729bf2228487441f",
+    "url": "/static/css/main.1dd5f368.chunk.css"
+  },
+  {
+    "revision": "85fba7b4451c447bd3e0",
+    "url": "/static/js/2.fc619aa9.chunk.js"
+  },
+  {
+    "revision": "574fc2bac5df0452b5a354632eb781c3",
+    "url": "/static/js/2.fc619aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a9729bf2228487441f",
+    "url": "/static/js/main.93c0124d.chunk.js"
+  },
+  {
+    "revision": "415039f72dffc77dd458",
+    "url": "/static/js/runtime-main.1e8db441.js"
+  }
+]);
